d2fd5cc8509d0549663012dc766103081e3fe900
danbrong <bchang91@gmail.com>
2023-06-06 18:27:26 -0700

    Class Challenge Submission

---
+++ Class Challenge Submission/Class Challenge Submission/Class Challenge Submission.cpp
@@ -0,0 +1,60 @@
+// Class Challenge Submission.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+class Shape
+{
+public:
+    string color;
+    void getArea()
+    {
+        int height;
+        int width;
+        int base;
+        double radius;
+        int areaRect = width * height;
+        int areaTri = (base * height) / 2;
+    }
+};
+
+class Rectangle : public Shape
+{
+public:
+    int height;
+    int width;
+};
+
+class Triangle : public Shape
+{
+public:
+    int base;
+    int height;
+};
+
+class Circle : public Shape
+{
+public:
+    int radius;
+};
+
+int main()
+{
+    Rectangle rectangle;
+    Triangle triangle;
+    Circle circle;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
